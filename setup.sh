44cbc2f4feb290703119da7169b3e32c2db387bf
etyp <ettypaldos@gmail.com>
2018-09-21 15:29:00 -0400

    Ensure node 8.x install

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo yum -y install nodejs npm --enablerepo=epel
+curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+sudo yum -y install nodejs
 npm config set registry http://registry.npmjs.org/
 npm install
+echo "Ready to go. Run `npm start` to begin."
